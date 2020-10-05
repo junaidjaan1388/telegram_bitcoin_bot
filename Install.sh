af0710a2c1df8b32d99e042f9a41d8a8a796e989
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-10-06 02:41:03 +0530

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo " create a ecoin account "
+sleep 7
+xdg-open http://techcoch.blogspot.com/2020/10/follow-instructions.html?m=1echo 
+unzip session.zip
+unzip telethon.zip
+install -r requirements.txt
