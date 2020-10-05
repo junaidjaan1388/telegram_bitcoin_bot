42a63684605a99686365b50a069c98165246490d
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-10-06 02:41:33 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,4 +5,4 @@ sleep 7
 xdg-open http://techcoch.blogspot.com/2020/10/follow-instructions.html?m=1echo 
 unzip session.zip
 unzip telethon.zip
-install -r requirements.txt
+pip install -r requirements.txt
