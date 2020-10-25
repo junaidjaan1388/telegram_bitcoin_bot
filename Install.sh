1d697f8a540a957ab8df583461eaaf589d306ba9
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-10-25 13:48:38 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 clear
-echo " create a ecoin account "
-sleep 7
-xdg-open http://techcoch.blogspot.com/2020/10/follow-instructions.html?m=1echo 
-unzip session.zip
+echo ""
+echo ""
+echo " SUBSCRIBE MY CHANNEL 🔔 " | lolcat
+xdg-open https://www.youtube.com/channel/UCiE0p7rXWBEncUVsLo1C5Xg
+echo ""unzip session.zip
 unzip telethon.zip
 pip install -r requirements.txt
