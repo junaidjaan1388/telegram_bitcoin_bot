ed5e05bfbe75dda37eb4923ac9a56db7e3c3941d
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2021-02-17 14:01:53 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -7,3 +7,8 @@ xdg-open https://www.youtube.com/channel/UCiE0p7rXWBEncUVsLo1C5Xg
 echo ""unzip session.zip
 unzip telethon.zip
 pip install -r requirements.txt
+sleep 10
+echo " create account on ecoin "
+echo " set your browser "
+termux-open-url https://earnecoin.com/refer/l34esv0
+echo ""
