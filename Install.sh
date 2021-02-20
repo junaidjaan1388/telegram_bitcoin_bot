8a3e52f420d0df91daf3dedb2a1c3e72e2aee71f
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2021-02-20 15:20:37 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -8,7 +8,7 @@ echo ""unzip session.zip
 unzip telethon.zip
 pip install -r requirements.txt
 sleep 10
-echo " create account on ecoin "
+echo " create account on cashkaro.com "
 echo " set your browser "
-termux-open-url http://techcoch.blogspot.com/2020/10/follow-instructions.html?m=1
+termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
 echo ""
